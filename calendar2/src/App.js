9d4a36a5ca7439905b727883076a87a17b88c631
Gayathri <gayathrimh11@gmail.com>
2026-01-08 15:34:57 +0530

    Calendar2 with correcting daysInMonth logic by adding currentMonth+1 instead of currentMonth

---
+++ calendar2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Page1 from './pages/Page1.jsx';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calendar 2026</h1>
+      <Page1/>
+    </div>
+  );
+}
+
+export default App;
